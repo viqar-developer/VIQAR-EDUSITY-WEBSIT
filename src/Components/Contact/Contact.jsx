a6a9b4964a3c0e38f3477ecb4a856e5370b5cb10
viqar-developer <csebatch0808@gmail.com>
2024-09-18 02:56:59 +0530

    first commit

---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import './Contact.css'
+import msg_icon from '../../assets/msg-icon.png'
+import mail_icon from '../../assets/mail-icon.png'
+import phone_icon from '../../assets/phone-icon.png'
+import location_icon from '../../assets/location-icon.png'
+import white_arrow from '../../assets/white-arrow.png'
+
+const Contact = () => {
+    const [result, setResult] = React.useState("");
+
+    const onSubmit = async (event) => {
+      event.preventDefault();
+      setResult("Sending....");
+      const formData = new FormData(event.target);
+  
+      formData.append("access_key", "47b3d77b-7f39-4b2a-98cb-a19bd88862a8");
+  
+      const response = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        body: formData
+      });
+  
+      const data = await response.json();
+  
+      if (data.success) {
+        setResult("Form Submitted Successfully");
+        event.target.reset();
+      } else {
+        console.log("Error", data);
+        setResult(data.message);
+      }
+    };
+    return(
+    <div className='contact'>
+        <div className="contact-col">
+            <h3>Send us a message <img src={msg_icon} alt="" /></h3>
+            <p>Feel free to reach out through contact from or find our contact
+                information below. your feedback,question,and suggestions are
+                important to us as we strive to provide exceptional service to our
+                university community.
+            </p>
+            <ul>
+                <li> <img src={mail_icon} alt="" />viqarahmed8912@gmail.com</li>
+                <li> <img src={phone_icon} alt="" />+1 123-456-7890</li>
+                <li> <img src={location_icon} alt="" />77 Massachusetts Ave,Cambridge,<br /> MA 02139,United State
+                </li>
+            </ul>
+        </div>
+        <div className="contact-col">
+            <form onSubmit={onSubmit}>
+                <label>Your Name</label>
+                <input type="text" name='name' placeholder='Enter your name' required />
+                <label>Phone Number</label>
+                <input type="tel" name='phone' placeholder='Enter your mobile number' required />
+                <label>Write your message here</label>
+                <textarea name="message" rows="6" placeholder='Enter your message' required />
+                <button type='submit' className='btn dark-btn'>Submit Now<img src={white_arrow} alt=""  /></button>
+
+            </form>
+            <span>{result}</span>
+        </div>
+
+      
+    </div>
+  )
+}
+
+export default Contact
